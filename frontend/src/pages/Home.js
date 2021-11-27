b958712823c3faa3b52d960106b976cd97fa70f5
hugojao <57982660+hugojao@users.noreply.github.com>
2021-11-27 13:55:06 +0100

    memeapiv0

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Meme from '../components/Meme';
 import Navbar from '../components/Navbar';
 
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Navbar/>
+            <Meme/>
         <h1>TESTs</h1>
         </div>
 
